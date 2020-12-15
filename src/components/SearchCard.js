8ca48490b624fd8f4450a10fcb8d94748eb62cf7
bodroghykristof <bodroghykristof@gmail.com>
2020-12-15 20:17:34 +0100

    Style titles on images

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -6,11 +6,22 @@ export default function SearchCard(props) {
     backgroundPosition: `center`,
     backgroundSize: `cover`,
     height: `100%`,
+    display: `flex`,
+    flexDirection: `column`,
+    justifyContent: `flex-end`,
+    paddingBottom: `10px`,
+  };
+
+  const titleStyle = {
+    border: `black 2px solid`,
+    background: `white`,
+    // minHeight: `30px`,
+    marginTop: `20px`,
   };
 
   return (
     <div style={style}>
-      <p>{props.data.strDrink}</p>
+      <p style={titleStyle}>{props.data.strDrink}</p>
     </div>
   );
 }
