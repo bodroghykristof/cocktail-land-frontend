31c47e240a33feb8b4d4a83b678f844ebac0a1f0
bodroghykristof <bodroghykristof@gmail.com>
2020-12-15 14:35:34 +0100

    Display results continuously

--- src/components/SearchCocktails.js
+++ src/components/SearchCocktails.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import ResultCard from './ResultCard';
+import ResultContainer from './ResultContainer';
 import { AllCocktailsContext } from './AllCocktailsContext';
 
 export const SearchCocktails = () => {
@@ -8,19 +8,25 @@ export const SearchCocktails = () => {
 
   const searchCocktailsByName = (event) => {
     const keyword = event.target.value;
+    let currentResult = [];
     for (let cocktail of allCocktails) {
-      setResultsByName([]);
       if (cocktail.strDrink.startsWith(keyword) && keyword !== '') {
-        console.log(cocktail.strDrink);
+        currentResult.push(cocktail);
       }
     }
+    setResultsByName(currentResult);
+    console.log(resultsByName);
   };
 
   return (
     <React.Fragment>
       <h1>Search Cocktails</h1>
       <input type='text' onChange={searchCocktailsByName}></input>
-      <ResultCard cocktails={resultsByName} />
+      {resultsByName.length > 0 ? (
+        <ResultContainer cocktails={resultsByName} />
+      ) : (
+        ``
+      )}
     </React.Fragment>
   );
 };
