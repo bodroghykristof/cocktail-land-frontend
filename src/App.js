bdad6cbd31b006dfa7da66164b93e56ca6e5b0ca
bodroghykristof <bodroghykristof@gmail.com>
2020-12-15 10:14:16 +0100

    Remove needless div

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import './App.css';
 import Header from './components/layout/Header';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { AllCocktailsProvider } from './components/AllCocktailsContext';
 import { Home } from './components/Home';
 import { SearchCocktails } from './components/SearchCocktails';
 
 const App = () => {
-
   let content = (
     <Router>
       <AllCocktailsProvider>
-        <div className="App">
-          <div className="container">
-            <Header/>
-            <Route exact path="/" component={ Home } />
-            <Route path="/search-cocktails" component={SearchCocktails} />
-          </div>
+        <div className='App'>
+          <Header />
+          <Route exact path='/' component={Home} />
+          <Route path='/search-cocktails' component={SearchCocktails} />
         </div>
       </AllCocktailsProvider>
     </Router>
   );
-  return content
-}
+  return content;
+};
 
 export default App;
