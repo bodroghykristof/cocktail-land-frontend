a23aa194bac546ccf605f92b56211ea2434068db
bodroghykristof <bodroghykristof@gmail.com>
2020-12-15 20:07:41 +0100

    Set background color for cards

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function SearchCard(props) {
-    return (
-        <div>
-            <p>{props.data.strDrink}</p>
-        </div>
-    )
+  const style = {
+    backgroundImage: `url(${props.data.strDrinkThumb})`,
+    backgroundPosition: `center`,
+    backgroundSize: `cover`,
+    height: `100%`,
+  };
+
+  return (
+    <div style={style}>
+      <p>{props.data.strDrink}</p>
+    </div>
+  );
 }
