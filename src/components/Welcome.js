47aaa7aa67ee61d64fbeabf9aa505c1feb243551
bodroghykristof <bodroghykristof@gmail.com>
2020-12-15 10:30:58 +0100

    Implement showing more message

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Welcome() {
+  const [verbose, setVerbose] = useState(false);
+
+  const showMore = () => {
+    setVerbose(true);
+  };
+
   return (
     <div>
       <h1>Welcome to Cocktail Land</h1>
@@ -10,13 +16,15 @@ export default function Welcome() {
         for underage visitors. If you are under 18 please, for your own good,
         leave this page!
       </p>
-      <button>More</button>
+      {verbose ? `` : <button onClick={showMore}>More</button>}
       <p>
-        Underage drinking is a serious public health problem in the United
+        {verbose
+          ? `Underage drinking is a serious public health problem in the United
         States. Alcohol is the most widely used substance among America’s youth,
         and drinking by young people poses enormous health and safety risks. The
         consequences of underage drinking can affect everyone—regardless of age
-        or drinking status.
+        or drinking status.`
+          : ``}
       </p>
       <p>Would you like to enter the page?</p>
       <button>Yes</button>
