26cd6c0f8bea6e1018197a1c3df0e4ba72c8a9ff
reClicked <gamper.renato@gmail.com>
2020-12-15 14:29:39 +0100

    Display random 10 cocktails on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,38 @@
-import React, { useContext } from 'react'; 
-import { AllCocktailsContext } from './AllCocktailsContext';
+import React, { useContext, useState } from "react";
+import { AllCocktailsContext } from "./AllCocktailsContext";
 
 export const Home = () => {
-
+    const [cocktails, setCocktails] = useState([]);
     const [allCocktails] = useContext(AllCocktailsContext);
-    
+    let cocktailCount = allCocktails.length;
+
+    const getRandomTen = () => {
+        let limit = 10;
+        let cocktailsArr = [];
+        while (0 < limit) {
+            let randomCocktail =
+                allCocktails[Math.floor(Math.random() * allCocktails.length)];
+            cocktailsArr.push(randomCocktail);
+            limit--;
+        }
+        setCocktails(cocktailsArr);
+    };
+
+    if (
+        allCocktails.length === cocktailCount &&
+        cocktails.length === 0 &&
+        cocktailCount > 0
+    ) {
+        getRandomTen();
+    }
+
     return (
-        <React.Fragment>
+        <div>
             <h1>Cocktails in your Life</h1>
-            <p>F***ck yeah</p>
-            <h3>{ allCocktails.length }</h3>
-        </React.Fragment>
-    )
-}
+            {cocktails.map((cocktail) => (
+                <div key={cocktail.idDrink}>{cocktail.strDrink}</div>
+            ))}
+            <h3>{allCocktails.length}</h3>
+        </div>
+    );
+};
