8a6f1db1ee5ce06a4aad45d6195d2a2fee72bf44
reClicked <gamper.renato@gmail.com>
2020-12-16 11:48:33 +0100

    Add link to items

--- src/components/ResultContainerItem.js
+++ src/components/ResultContainerItem.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
-import DynamicScroller from "./scrolling/DynamicScroller";
+import { Link } from "react-router-dom";
 import SearchCard from "./SearchCard";
+import DynamicScroller from "./scrolling/DynamicScroller";
 
 export default function ResultContainerItem({ cocktails, search }) {
     return (
@@ -10,7 +11,15 @@ export default function ResultContainerItem({ cocktails, search }) {
                     <h3>Alcoholic cocktails by {search}:</h3>
                     <DynamicScroller>
                         {cocktails.map((cocktail) => (
-                            <SearchCard data={cocktail} />
+                            <Link
+                                to={`cocktail/${cocktail.idDrink}`}
+                                style={{
+                                    textDecoration: "none",
+                                    color: "black",
+                                }}
+                            >
+                                <SearchCard data={cocktail} />
+                            </Link>
                         ))}
                     </DynamicScroller>
                 </Fragment>
