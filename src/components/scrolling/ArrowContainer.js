484b50587656451ed4dfd9ffc588450bd8a72679
reClicked <gamper.renato@gmail.com>
2020-12-16 14:33:40 +0100

    Move arrows to center with flex

--- src/components/scrolling/ArrowContainer.js
+++ src/components/scrolling/ArrowContainer.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const ArrowContainer = styled.div`
-  padding-top: 100px;
-  display: flex;
-  align-content: center;
-  justify-content: center;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
 `;
 
 export default ArrowContainer;
