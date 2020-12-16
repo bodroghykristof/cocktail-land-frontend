1dbba340287c070b24cfc763ca5184ecdadee29e
bodroghykristof <bodroghykristof@gmail.com>
2020-12-16 12:10:31 +0100

    Add flamingos to welcome page

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -4,6 +4,8 @@ import WelcomeHeader from './WelcomeHeader';
 import WarningIcon from '@material-ui/icons/Warning';
 import { Link } from 'react-router-dom';
 import './welcome.scss';
+import flamingo from '../../static/flamingo-nobg.png';
+import flamingoReverse from '../../static/flamingo-nobg-reverse.png';
 
 export default function Welcome() {
   const [verbose, setVerbose] = useState(false);
@@ -19,35 +21,39 @@ export default function Welcome() {
   return (
     <Fragment>
       <WelcomeHeader>Welcome to Cocktail Land</WelcomeHeader>
-      <WelcomeBox>
-        <div className='box-header'>
-          <WarningIcon style={iconStyle} />
-          <h2>Warning</h2>
-          <WarningIcon style={iconStyle} />
-        </div>
-        <p>
-          This site offers a huge variety of alcoholic drinks, being not
-          suitable for underage visitors. If you are under 18 please, for your
-          own good, leave this page!
-        </p>
-        {verbose ? `` : <button onClick={showMore}>More</button>}
-        <p className='details'>
-          {verbose
-            ? `Underage drinking is a serious public health problem in the United
+      <div className='flamingo-wrapper'>
+        <img src={flamingoReverse} alt='flamingo' height='450px'></img>
+        <WelcomeBox>
+          <div className='box-header'>
+            <WarningIcon style={iconStyle} />
+            <h2>Warning</h2>
+            <WarningIcon style={iconStyle} />
+          </div>
+          <p>
+            This site offers a huge variety of alcoholic drinks, being not
+            suitable for underage visitors. If you are under 18 please, for your
+            own good, leave this page!
+          </p>
+          {verbose ? `` : <button onClick={showMore}>More</button>}
+          <p className='details'>
+            {verbose
+              ? `Underage drinking is a serious public health problem in the United
         States and most of Europe. Alcohol is the most widely used substance among youth,
         and drinking by young people poses enormous health and safety risks. The
         consequences of underage drinking can affect everyone - regardless of age
         or drinking status.`
-            : ``}
-        </p>
-        <p>Would you like to enter the page?</p>
-        <Link to='/home'>
-          <button>Yes</button>
-        </Link>
-        <a href='https://www.sesamestreet.org/'>
-          <button>No</button>
-        </a>
-      </WelcomeBox>
+              : ``}
+          </p>
+          <p>Would you like to enter the page?</p>
+          <Link to='/home'>
+            <button>Yes</button>
+          </Link>
+          <a href='https://www.sesamestreet.org/'>
+            <button>No</button>
+          </a>
+        </WelcomeBox>
+        <img src={flamingo} alt='flamingo' height='450px'></img>
+      </div>
     </Fragment>
   );
 }
