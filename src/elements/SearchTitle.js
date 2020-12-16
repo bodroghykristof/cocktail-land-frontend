fd5b19acfbd3aa7af29e7a3ab94ac34c4b40ce2e
reClicked <gamper.renato@gmail.com>
2020-12-16 16:33:45 +0100

    Create styled component for search title h3 tag

---
+++ src/elements/SearchTitle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const SearchTitle = styled.h3`
+    border: 3px solid #ebca6c;
+    border-radius: 6px;
+    background-color: rgba(238, 212, 142, 0.7);
+
+    width: 23%;
+
+    margin: auto;
+    padding: 10px;
+`;
+
+export default SearchTitle;
