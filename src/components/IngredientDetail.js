06e9375b8fc927c18e4a98ce00e7e36d29224020
bodroghykristof <bodroghykristof@gmail.com>
2020-12-17 17:46:03 +0100

    Make title a general H1

--- src/components/IngredientDetail.js
+++ src/components/IngredientDetail.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import "../components/css/ingredientDetail.scss";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React, { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+import '../components/css/ingredientDetail.scss';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { H1 } from './HomeDesign';
 
-import FreeBreakfastIcon from "@material-ui/icons/FreeBreakfast";
-import LocalBarIcon from "@material-ui/icons/LocalBar";
+import FreeBreakfastIcon from '@material-ui/icons/FreeBreakfast';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
 
 const IngredientDetail = () => {
   const [ingredients, setIngredients] = useState([]);
@@ -25,12 +26,10 @@ const IngredientDetail = () => {
   }
 
   return (
-    <div className="ingredient-detail">
-      {console.log(ingredients)}
-      <h1 className="ingredient-title">
-        Ingredient: {ingredients[0].strIngredient}
-      </h1>
-      {ingredients[0].strDescription !== null ? (
+    <Fragment>
+      <H1 className='ingredient-title'>{ingredients[0].strIngredient}</H1>
+      <div className='ingredient-detail'>
+        {/* {ingredients[0].strDescription !== null ? (
         <div className="ingredient-description-container">
           <div className="ingredient-description-text">
             {ingredients[0].strDescription}
@@ -38,44 +37,45 @@ const IngredientDetail = () => {
         </div>
       ) : (
         ""
-      )}
+      )} */}
 
-      {ingredients[0].strAlcohol === "No" ? (
-        <h1 className="ingredient-alcohol">
-          <span>
-            <FreeBreakfastIcon />
-          </span>
-          Alcohol:
-          {ingredients[0].strAlcohol}
-        </h1>
-      ) : ingredients[0].strAlcohol === "Yes" ? (
-        <h1 className="ingredient-not-alcohol">
-          <span>
-            <LocalBarIcon />
-          </span>
-          Alcohol:
-          {ingredients[0].strAlcohol}
-        </h1>
-      ) : (
-        <h1 className="ingredient-alcohol">
-          <span>
-            <FreeBreakfastIcon />
-          </span>
-          Alcohol: Who knows
-        </h1>
-      )}
+        {ingredients[0].strAlcohol === 'No' ? (
+          <h1 className='ingredient-alcohol'>
+            <span>
+              <FreeBreakfastIcon />
+            </span>
+            Alcohol:
+            {ingredients[0].strAlcohol}
+          </h1>
+        ) : ingredients[0].strAlcohol === 'Yes' ? (
+          <h1 className='ingredient-not-alcohol'>
+            <span>
+              <LocalBarIcon />
+            </span>
+            Alcohol:
+            {ingredients[0].strAlcohol}
+          </h1>
+        ) : (
+          <h1 className='ingredient-alcohol'>
+            <span>
+              <FreeBreakfastIcon />
+            </span>
+            Alcohol: Who knows
+          </h1>
+        )}
 
-      {ingredients[0].strABV === null ? (
-        <h1 className="ingredient-alcohol">
-          0<span>%</span>
-        </h1>
-      ) : (
-        <h1 className="ingredient-not-alcohol">
-          {ingredients[0].strABV}
-          <span>%</span>
-        </h1>
-      )}
-    </div>
+        {ingredients[0].strABV === null ? (
+          <h1 className='ingredient-alcohol'>
+            0<span>%</span>
+          </h1>
+        ) : (
+          <h1 className='ingredient-not-alcohol'>
+            {ingredients[0].strABV}
+            <span>%</span>
+          </h1>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
