c959a45192c638b3a8c9202df171a48a2384afb2
Szkini22 <tamas.catanzaro98@gmail.com>
2021-01-12 16:46:30 +0100

    form created with bootstrap and added style

--- src/components/users/Register.js
+++ src/components/users/Register.js
@@ -1,11 +1,14 @@
-import React, { useState, useRef } from 'react';
-import { useHistory } from 'react-router-dom';
-import UserFormContainer from './UserFormContainer';
-import RegisterButton from '../layout/Button';
-import apiService from '../services/Api';
+import React, { useState, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import UserFormContainer from "./UserFormContainer";
+import RegisterButton from "../layout/Button";
+import apiService from "../services/Api";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/registrationpage.scss";
 
 function Register() {
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const userName = useRef(null);
   const email = useRef(null);
   const passwordOne = useRef(null);
@@ -19,18 +22,18 @@ function Register() {
     const passwordTwoInput = passwordTwo.current.value;
 
     if (
-      userNameInput === '' ||
-      emailInput === '' ||
-      passwordOneInput === '' ||
-      passwordTwoInput === ''
+      userNameInput === "" ||
+      emailInput === "" ||
+      passwordOneInput === "" ||
+      passwordTwoInput === ""
     ) {
-      setErrorMessage('Please fill out all input fields!');
-    } else if (!RegExp('^[^@.]+@[^@.]+\\.[^@.]+$').test(emailInput)) {
-      setErrorMessage('Invalid email!');
+      setErrorMessage("Please fill out all input fields!");
+    } else if (!RegExp("^[^@.]+@[^@.]+\\.[^@.]+$").test(emailInput)) {
+      setErrorMessage("Invalid email!");
     } else if (passwordOneInput !== passwordTwoInput) {
-      setErrorMessage('Mismatching passwords!');
+      setErrorMessage("Mismatching passwords!");
     } else if (passwordOneInput.length < 5) {
-      setErrorMessage('Password must be at least 5 characters!');
+      setErrorMessage("Password must be at least 5 characters!");
     } else {
       const user = {
         name: userNameInput,
@@ -40,33 +43,74 @@ function Register() {
       const response = await apiService.register(user);
       console.log(response);
       if (response.status === 409) setErrorMessage(response.data.message);
-      else history.push('/login');
+      else history.push("/login");
     }
   };
 
   return (
-    <UserFormContainer>
-      <input type='text' placeholder='Username' ref={userName}></input>
-      <br></br>
-      <input type='email' placeholder='Email' ref={email}></input>
-      <br></br>
-      <input type='password' placeholder='Password' ref={passwordOne}></input>
-      <br></br>
-      <input
-        type='password'
-        placeholder='Password Again'
-        ref={passwordTwo}
-      ></input>
-      <br></br>
-      <p className='error-message'>{errorMessage}</p>
-      <br></br>
-      <RegisterButton
-        id='loginConfirmationButton'
-        onClick={(e) => checkRegistration(e)}
-      >
-        Register
-      </RegisterButton>
-    </UserFormContainer>
+    <div className="registration-page">
+      {/* <UserFormContainer>
+        <input type="text" placeholder="Username" ref={userName}></input>
+        <br></br>
+        <input type="email" placeholder="Email" ref={email}></input>
+        <br></br>
+        <input type="password" placeholder="Password" ref={passwordOne}></input>
+        <br></br>
+        <input
+          type="password"
+          placeholder="Password Again"
+          ref={passwordTwo}
+        ></input>
+        <br></br>
+        <p className="error-message">{errorMessage}</p>
+        <br></br>
+        <RegisterButton
+          id="loginConfirmationButton"
+          onClick={(e) => checkRegistration(e)}
+        >
+          Register
+        </RegisterButton>
+      </UserFormContainer> */}
+
+      {/* <div className="login-form">
+  <input type="text" placeholder="Username" ref={email}></input>
+  <input type="password" placeholder="Password" ref={password}></input>
+  <p>{errorMessage}</p>
+  <button id="loginConfirmationButton" onClick={checkLogin}>
+    Login
+  </button>
+</div> */}
+
+      <Form className="registration-form">
+      <Form.Group controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" ref={userName} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" ref={email} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" ref={passwordOne} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password Again</Form.Label>
+          <Form.Control type="password" placeholder="Password again" ref={passwordTwo} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicError">
+          <Form.Text>{errorMessage}</Form.Text>
+        </Form.Group>
+
+        <Button variant="secondary" type="button" onClick={(e) => checkRegistration(e)}>
+          Register
+        </Button>
+      </Form>
+    </div>
   );
 }
 
