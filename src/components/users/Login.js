55a3c49236d9846a47deab42afe94deba67028f0
bodroghykristof <bodroghykristof@gmail.com>
2021-01-12 10:48:16 +0100

    Add basic of forms

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import UserFormContainer from './UserFormContainer';
 
 function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  const [errorMessage, setErrorMessage] = useState('');
+  const userName = useRef(null);
+  const password = useRef(null);
+
+  const forgotPasswordAction = () =>
+    alert('Shame on you, we do not have any smart solution for this yet');
+
+  const checkLogin = () => {
+    const userNameInput = userName.current.value;
+    const passwordInput = password.current.value;
+
+    if (userNameInput === '' || passwordInput === '') {
+      setErrorMessage('Please fill out all input fields!');
+    } else {
+      window.location.replace('/test');
+    }
+  };
+
+  return (
+    <UserFormContainer>
+      <input type='text' placeholder='Username' ref={userName}></input>
+      <br></br>
+      <input type='password' placeholder='Password' ref={password}></input>
+      <br></br>
+      <p>{errorMessage}</p>
+      <p id='forgotPassword' onClick={() => forgotPasswordAction()}>
+        Forgot my password
+      </p>
+      <button id='loginConfirmationButton' onClick={checkLogin}>
+        Login
+      </button>
+    </UserFormContainer>
+  );
 }
 
-export default Login
+export default Login;
--- src/components/users/Register.js
+++ src/components/users/Register.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import UserFormContainer from './UserFormContainer';
 
 function Register() {
-    return (
-        <div>
-            Register
-        </div>
-    )
+  const [errorMessage, setErrorMessage] = useState('');
+  const userName = useRef(null);
+  const passwordOne = useRef(null);
+  const passwordTwo = useRef(null);
+
+  const checkRegistration = () => {
+    const userNameInput = userName.current.value;
+    const passwordOneInput = passwordOne.current.value;
+    const passwordTwoInput = passwordTwo.current.value;
+
+    if (
+      userNameInput === '' ||
+      passwordOneInput === '' ||
+      passwordTwoInput === ''
+    ) {
+      setErrorMessage('Please fill out all input fields!');
+    } else if (passwordOneInput !== passwordTwoInput) {
+      setErrorMessage('Mismatching passwords!');
+    } else {
+      window.location.replace('/test');
+    }
+  };
+
+  return (
+    <UserFormContainer>
+      <input type='text' placeholder='Username' ref={userName}></input>
+      <br></br>
+      <input type='password' placeholder='Password' ref={passwordOne}></input>
+      <br></br>
+      <input
+        type='password'
+        placeholder='Password Again'
+        ref={passwordTwo}
+      ></input>
+      <br></br>
+      <p>{errorMessage}</p>
+      <br></br>
+      <button
+        id='loginConfirmationButton'
+        onClick={(e) => checkRegistration(e)}
+      >
+        Register
+      </button>
+    </UserFormContainer>
+  );
 }
 
-export default Register
+export default Register;
