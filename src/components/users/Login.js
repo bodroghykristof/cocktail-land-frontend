c2c906738c1c80eb84e11036f8bc0e7d8687b45e
bodroghykristof <bodroghykristof@gmail.com>
2021-01-12 15:45:28 +0100

    implement calling /login endpoint on server and saving token into local storage

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -1,28 +1,46 @@
 import React, { useState, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import UserFormContainer from './UserFormContainer';
+import apiService from '../services/Api';
 
 function Login() {
   const [errorMessage, setErrorMessage] = useState('');
-  const userName = useRef(null);
+  const email = useRef(null);
   const password = useRef(null);
+  const history = useHistory();
 
   const forgotPasswordAction = () =>
     alert('Shame on you, we do not have any smart solution for this yet');
 
-  const checkLogin = () => {
-    const userNameInput = userName.current.value;
+  const checkLogin = async () => {
+    const emailInput = email.current.value;
     const passwordInput = password.current.value;
 
-    if (userNameInput === '' || passwordInput === '') {
+    if (emailInput === '' || passwordInput === '') {
       setErrorMessage('Please fill out all input fields!');
+    } else if (
+      !RegExp('^[^@.]+@[^@.]+\\.[^@.]+$').test(emailInput) ||
+      passwordInput.length < 5
+    ) {
+      setErrorMessage('Invalid email or password');
     } else {
-      window.location.replace('/test');
+      const user = {
+        email: emailInput,
+        password: passwordInput,
+      };
+
+      const response = await apiService.login(user);
+      if (response.status === 401) setErrorMessage(response.data.message);
+      else {
+        localStorage.setItem('token', response.data.token);
+        history.push('/home');
+      }
     }
   };
 
   return (
     <UserFormContainer>
-      <input type='text' placeholder='Username' ref={userName}></input>
+      <input type='text' placeholder='Email' ref={email}></input>
       <br></br>
       <input type='password' placeholder='Password' ref={password}></input>
       <br></br>
