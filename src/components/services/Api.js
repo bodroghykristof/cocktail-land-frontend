b63475dc7da7ec6be15cb4280e1920478d54094c
reClicked <gamper.renato@gmail.com>
2021-01-14 11:27:48 +0100

    Add logout method

--- src/components/services/Api.js
+++ src/components/services/Api.js
@@ -3,28 +3,31 @@ import Axios from "axios";
 const URL = "http://127.0.0.1:8000/api";
 
 const apiService = {
-  register: (user) =>
-    Axios.post(`${URL}/register`, user).catch((error) => error.response),
+    register: (user) =>
+        Axios.post(`${URL}/register`, user).catch((error) => error.response),
 
-  login: (user) =>
-    Axios.post(`${URL}/login`, user).catch((error) => error.response),
+    login: (user) =>
+        Axios.post(`${URL}/login`, user).catch((error) => error.response),
 
-  getFavoriteCoctails: (token) =>
-    Axios.get(`${URL}/favourite`, {
-      headers: { Authorization: `Bearer ${token}` },
-    }).catch((error) => error.response),
+    logout: (token) =>
+        Axios.post(`${URL}/logout`, {
+            headers: { Authorization: `Bearer ${token}` },
+        }).catch((error) => error.response),
 
-  addToFavourite: (token, cocktail) =>
-    Axios.post(
-      `${URL}/favourite`,
-      cocktail,
-      {headers: { Authorization: `Bearer ${token}` }}
-    ).catch((error) => error.response),
+    getFavoriteCoctails: (token) =>
+        Axios.get(`${URL}/favourite`, {
+            headers: { Authorization: `Bearer ${token}` },
+        }).catch((error) => error.response),
 
-  deleteFromFavourite: (token, id) =>
-    Axios.delete(`${URL}/favourite/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    }).catch((error) => error.response),
+    addToFavourite: (token, cocktail) =>
+        Axios.post(`${URL}/favourite`, cocktail, {
+            headers: { Authorization: `Bearer ${token}` },
+        }).catch((error) => error.response),
+
+    deleteFromFavourite: (token, id) =>
+        Axios.delete(`${URL}/favourite/${id}`, {
+            headers: { Authorization: `Bearer ${token}` },
+        }).catch((error) => error.response),
 };
 
 export default apiService;
