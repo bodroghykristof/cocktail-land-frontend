dda5c2af16f21d82a499abec2ba73db2fddc93d1
bodroghykristof <bodroghykristof@gmail.com>
2021-01-14 10:51:13 +0100

    fix icon heart on detail page

--- src/components/FavoriteIconHearth.js
+++ src/components/FavoriteIconHearth.js
@@ -4,28 +4,26 @@ import { IconButton } from '@material-ui/core';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import { favoriteIconStyle } from './CocktailCardDesign';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import apiService from "./services/Api";
+import apiService from './services/Api';
 
 export default function FavoriteIconHearth(props) {
-
   const [favorites, setFavorites] = useContext(FavoritesContext);
   const [value, setValue] = useState(props.value);
   const cocktail = props.cocktail;
 
   useEffect(() => {
-
     const getCardValue = (cocktailName) => {
-      if ( props.value !== undefined) {
+      if (props.value !== undefined) {
         setValue(value);
       } else {
-        setValue(favorites.map(favorite => favorite.strDrink).includes(cocktailName));
+        setValue(
+          favorites.map((favorite) => favorite.strDrink).includes(cocktailName)
+        );
       }
     };
 
     getCardValue(cocktail.strDrink);
-
   }, [favorites, cocktail.strDrink, value, props.value]);
-
 
   const onClickHeart = (e) => {
     if (value === false) {
@@ -39,36 +37,43 @@ export default function FavoriteIconHearth(props) {
     const cocktailData = {
       strDrink: cocktail.strDrink,
       idDrink: cocktail.idDrink,
-      strDrinkThumb: cocktail.strDrinkThumb
-    } 
+      strDrinkThumb: cocktail.strDrinkThumb,
+    };
     e.preventDefault();
-    await apiService.addToFavourite(localStorage.getItem("token"), cocktailData);
-    setFavorites((prevFavorites) => [
-      ...prevFavorites,
+    await apiService.addToFavourite(
+      localStorage.getItem('token'),
       cocktailData
-    ]);
+    );
     setValue(true);
+    setFavorites((prevFavorites) => [...prevFavorites, cocktailData]);
   };
 
   const deleteFavorite = async (e) => {
     e.preventDefault();
-    await apiService.deleteFromFavourite(localStorage.getItem("token"), cocktail.idDrink);
+    await apiService.deleteFromFavourite(
+      localStorage.getItem('token'),
+      cocktail.idDrink
+    );
     setValue(false);
     const updatedFavorites = favorites.filter(
       (favCocktail) => favCocktail.idDrink !== cocktail.idDrink
     );
     setFavorites(updatedFavorites);
   };
-  
+
   return (
-    <IconButton 
-      onClick={(e) => {onClickHeart(e);}}
+    <IconButton
+      onClick={(e) => {
+        onClickHeart(e);
+      }}
       name={cocktail.strDrink}
       value={value}
-    > {value ? (
-        <FavoriteIcon style={favoriteIconStyle}/>
+    >
+      {' '}
+      {value ? (
+        <FavoriteIcon style={favoriteIconStyle} />
       ) : (
-        <FavoriteBorderOutlinedIcon style={favoriteIconStyle}/>
+        <FavoriteBorderOutlinedIcon style={favoriteIconStyle} />
       )}
     </IconButton>
   );
