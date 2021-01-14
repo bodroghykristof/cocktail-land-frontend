602d9a0e5af3c65b2cfcd4d38743922ac8830174
bodroghykristof <bodroghykristof@gmail.com>
2021-01-14 14:57:05 +0100

    fetch all cocktails using a single request

--- src/components/services/Api.js
+++ src/components/services/Api.js
@@ -1,6 +1,6 @@
-import Axios from "axios";
+import Axios from 'axios';
 
-const URL = "http://127.0.0.1:8000/api";
+const URL = 'http://127.0.0.1:8000/api';
 
 const apiService = {
   register: (user) =>
@@ -9,17 +9,20 @@ const apiService = {
   login: (user) =>
     Axios.post(`${URL}/login`, user).catch((error) => error.response),
 
+  getAllCocktails: (token) =>
+    Axios.get(`${URL}/get-all-cocktails`, {
+      headers: { Authorization: `Bearer ${token}` },
+    }).catch((error) => error.response),
+
   getFavoriteCoctails: (token) =>
     Axios.get(`${URL}/favourite`, {
       headers: { Authorization: `Bearer ${token}` },
     }).catch((error) => error.response),
 
   addToFavourite: (token, cocktail) =>
-    Axios.post(
-      `${URL}/favourite`,
-      cocktail,
-      {headers: { Authorization: `Bearer ${token}` }}
-    ).catch((error) => error.response),
+    Axios.post(`${URL}/favourite`, cocktail, {
+      headers: { Authorization: `Bearer ${token}` },
+    }).catch((error) => error.response),
 
   deleteFromFavourite: (token, id) =>
     Axios.delete(`${URL}/favourite/${id}`, {
