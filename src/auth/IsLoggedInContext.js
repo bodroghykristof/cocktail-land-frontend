d4fb6e3bb523e646a658d90e9c0553d356aed124
Szkini22 <tamas.catanzaro98@gmail.com>
2021-01-14 12:11:11 +0100

    initial state is set to boolean according to token

--- src/auth/IsLoggedInContext.js
+++ src/auth/IsLoggedInContext.js
@@ -2,8 +2,10 @@ import React, { useState, createContext } from "react";
 
 export const IsLoggedInContext = createContext();
 
+const isTokenInLS = localStorage.getItem("token") ? true : false;
+
 export const IsLoggedInProvider = (props) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(isTokenInLS);
 
   return (
     <IsLoggedInContext.Provider value={[isLoggedIn, setIsLoggedIn]}>
