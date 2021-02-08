24e955b286aa44e5fbe8ec3dcc3f2420ffc656ab
reClicked <gamper.renato@gmail.com>
2021-02-08 16:08:58 +0100

    Create about components

---
+++ src/components/AboutDesign.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+    color: #2a2a2b;
+    margin: 20px 0;
+    font-size: 29px;
+    font-weight: bold;
+`;
+
+export const DIV = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    margin: auto;
+    margin-top: 150px;
+    padding: 20px 30px;
+
+    border: solid #2a2a2b 3px;
+    background-color: #b4b7c7;
+    border-radius: 7px;
+    max-width: 500px;
+    height: 300px;
+`;
