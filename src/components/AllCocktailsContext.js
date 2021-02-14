336af84dd49afb4244b17c3260d3a74cf0728848
CocoBloom <virag.baranyai2@gmail.com>
2021-02-14 11:58:47 +0100

    add new version of sending 26 request in React

--- src/components/AllCocktailsContext.js
+++ src/components/AllCocktailsContext.js
@@ -6,14 +6,25 @@ export const AllCocktailsContext = createContext();
 export const AllCocktailsProvider = (props) => {
   const [allCocktails, setAllCocktails] = useState([]);
 
-  useEffect(() => {
-    async function fetchCocktails() {
-      const token = localStorage.getItem('token');
-      const result = await apiService.getAllCocktails(token);
-      setAllCocktails(result.data.cocktails);
+  
+useEffect( () => {
+
+  async function fetching () {
+    let cocktail = [];
+    for (let i = 97; i < 122; i++) {
+      let url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + String.fromCharCode(i);
+      console.log("url: ", url);
+      await Axios.get(url)
+          .then(res => res.data)
+          .then(res =>res['drinks'].forEach(e => cocktail.push(e)) )
+          .catch(error => console.log(error))
     }
-    fetchCocktails();
-  }, []);
+    setAllCocktails(cocktail);
+  }
+
+  fetching();
+
+}, [])
 
   return (
     <AllCocktailsContext.Provider value={[allCocktails, setAllCocktails]}>
