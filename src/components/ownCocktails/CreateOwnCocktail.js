717b3e06467a7d6413b9844976d024499facba06
reClicked <gamper.renato@gmail.com>
2021-02-23 11:06:08 +0100

    Create compnent to make your own cocktails

---
+++ src/components/ownCocktails/CreateOwnCocktail.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { DIV, H1 } from "../HomeDesign";
+
+export const CreateOwnCocktail = () => {
+    return (
+        <Fragment>
+            <H1>Create your cocktail</H1>
+        </Fragment>
+    );
+};
+
+export default CreateOwnCocktail;
