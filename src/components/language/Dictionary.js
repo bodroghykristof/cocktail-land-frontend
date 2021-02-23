a098a69daf93708e50b68a2a76baf802597958a8
reClicked <gamper.renato@gmail.com>
2021-02-23 10:11:50 +0100

    Add mine to dictionary to translate

--- src/components/language/Dictionary.js
+++ src/components/language/Dictionary.js
@@ -1,81 +1,87 @@
 const dictionary = {
-  headerTitle: {
-    english: 'Find your Favourite Cocktails!',
-    german: 'Finden Sie Ihre Lieblingscocktails!',
-  },
-  search: {
-    english: 'Search',
-    german: 'Suchen',
-  },
-  favourites: {
-    english: 'Favourites',
-    german: 'Favoriten',
-  },
-  about: {
-    english: 'About',
-    german: 'Über',
-  },
-  register: {
-    english: 'Register',
-    german: 'Registieren',
-  },
-  homeTitle: {
-    english: 'Cocktails in your Life',
-    german: 'Cocktails in Unserem Leben',
-  },
-  add: {
-    english: 'Add',
-    german: 'Sammeln',
-  },
-  delete: {
-    english: 'Delete',
-    german: 'Entfernen',
-  },
-  searchTitle: {
-    english: 'Search Cocktails',
-    german: 'Cocktailsuchen',
-  },
-  searchPlaceholder: {
-    english: 'Find your cocktail...',
-    german: 'Finde deinen Cocktail...',
-  },
-  alcoholic: {
-    english: 'Alcoholic',
-    german: 'Alholische',
-  },
-  alcoholFree: {
-    english: 'Virgin',
-    german: 'Alkoholfrei',
-  },
-  name: {
-    english: 'Name',
-    german: 'Namen',
-  },
-  ingredient: {
-    english: 'Ingredients',
-    german: 'Zutaten',
-  },
-  searchHeaderMiddle: {
-    english: 'cocktails by',
-    german: 'Cocktails gefunden mit',
-  },
-  noFavourite: {
-    english: 'You have no favourite cocktails yet',
-    german: 'Sie haben keine lieblingsCocktail gewählen',
-  },
-  instructions: {
-    english: 'Instructions',
-    german: 'Anweisungen',
-  },
-  howTo: {
-    english: "How it's made",
-    german: 'Wie ist es gemacht?',
-  },
+    headerTitle: {
+        english: "Find your Favourite Cocktails!",
+        german: "Finden Sie Ihre Lieblingscocktails!",
+    },
+    search: {
+        english: "Search",
+        german: "Suchen",
+    },
+    favourites: {
+        english: "Favourites",
+        german: "Favoriten",
+    },
+    about: {
+        english: "About",
+        german: "Über",
+    },
+    register: {
+        english: "Register",
+        german: "Registieren",
+    },
+    homeTitle: {
+        english: "Cocktails in your Life",
+        german: "Cocktails in Unserem Leben",
+    },
+    add: {
+        english: "Add",
+        german: "Sammeln",
+    },
+    delete: {
+        english: "Delete",
+        german: "Entfernen",
+    },
+    searchTitle: {
+        english: "Search Cocktails",
+        german: "Cocktailsuchen",
+    },
+    searchPlaceholder: {
+        english: "Find your cocktail...",
+        german: "Finde deinen Cocktail...",
+    },
+    alcoholic: {
+        english: "Alcoholic",
+        german: "Alholische",
+    },
+    alcoholFree: {
+        english: "Virgin",
+        german: "Alkoholfrei",
+    },
+    name: {
+        english: "Name",
+        german: "Namen",
+    },
+    ingredient: {
+        english: "Ingredients",
+        german: "Zutaten",
+    },
+    searchHeaderMiddle: {
+        english: "cocktails by",
+        german: "Cocktails gefunden mit",
+    },
+    noFavourite: {
+        english: "You have no favourite cocktails yet",
+        german: "Sie haben keine lieblingsCocktail gewählen",
+    },
+    instructions: {
+        english: "Instructions",
+        german: "Anweisungen",
+    },
+    howTo: {
+        english: "How it's made",
+        german: "Wie ist es gemacht?",
+    },
 
-  aboutText: {
-    english: "This application is for cocktail enthusiasts whose favorite free time activity is to make their own cocktails. You have the opportunity to look up different kinds of coktails and you can also pick your favorite ones and save them for later. This page also provides english and german version. In the future we are plannig to extend our functionality stack so that you can create your own cocktails!",
-    german: "Diese Anwendung ist für Cocktail-Enthusiasten gedacht, deren Lieblingsbeschäftigung in der Freizeit darin besteht, ihre eigenen Cocktails zuzubereiten. Sie haben die Möglichkeit, verschiedene Arten von Coktails nachzuschlagen und Ihre Favoriten auszuwählen und für später aufzubewahren. Diese Seite bietet auch englische und deutsche Version. In Zukunft planen wir, unseren Funktionsstapel zu erweitern, damit Sie Ihre eigenen Cocktails kreieren können!",
-  }
+    aboutText: {
+        english:
+            "This application is for cocktail enthusiasts whose favorite free time activity is to make their own cocktails. You have the opportunity to look up different kinds of coktails and you can also pick your favorite ones and save them for later. This page also provides english and german version. In the future we are plannig to extend our functionality stack so that you can create your own cocktails!",
+        german:
+            "Diese Anwendung ist für Cocktail-Enthusiasten gedacht, deren Lieblingsbeschäftigung in der Freizeit darin besteht, ihre eigenen Cocktails zuzubereiten. Sie haben die Möglichkeit, verschiedene Arten von Coktails nachzuschlagen und Ihre Favoriten auszuwählen und für später aufzubewahren. Diese Seite bietet auch englische und deutsche Version. In Zukunft planen wir, unseren Funktionsstapel zu erweitern, damit Sie Ihre eigenen Cocktails kreieren können!",
+    },
+    ownCocktails: {
+        english: "Mine",
+        german: "Meine",
+    },
 };
 
 export default dictionary;
