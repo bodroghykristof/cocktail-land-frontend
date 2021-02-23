5579221bc4b3821ffb3e1eedb9f18b028f8226ad
reClicked <gamper.renato@gmail.com>
2021-02-23 10:30:38 +0100

    Create own cocktail component

---
+++ src/components/ownCocktails/OwnCocktail.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { DIV, H1 } from "../HomeDesign";
+
+export default function ownCocktail() {
+    let content = `<p>Loading your handmade Cocktails.....</p>`;
+
+    return (
+        <Fragment>
+            <H1>Own cocktails</H1>
+        </Fragment>
+    );
+}
