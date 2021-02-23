50f4f46864852c5493d34d207ca64b59e26f0fa8
reClicked <gamper.renato@gmail.com>
2021-02-23 13:42:43 +0100

    Create formdiv styled component

--- src/components/HomeDesign.js
+++ src/components/HomeDesign.js
@@ -10,3 +10,11 @@ export const DIV = styled.div`
     flex-wrap: wrap;
     justify-content: center;
 `;
+
+export const FORMDIV = styled.div`
+    display: flex;
+    justify-content: center;
+    /* align-items: center; */
+
+    margin: auto;
+`;
