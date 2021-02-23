c7fa99ef50d01e2da8d96626056e57ce01c82e49
reClicked <gamper.renato@gmail.com>
2021-02-23 11:06:56 +0100

    Add link that redirects to creating your own cocktail page

--- src/components/ownCocktails/OwnCocktail.js
+++ src/components/ownCocktails/OwnCocktail.js
@@ -1,12 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { DIV, H1 } from "../HomeDesign";
+import { LanguageContext } from "../language/LanguageContext";
+import dictionary from "../language/Dictionary";
+import { Link } from "react-router-dom";
 
-export default function ownCocktail() {
+export const OwnCocktail = () => {
+    const [language] = useContext(LanguageContext);
+
     let content = `<p>Loading your handmade Cocktails.....</p>`;
 
     return (
         <Fragment>
             <H1>Own cocktails</H1>
+            <div>
+                <Link className="link" to="/create-own">
+                    {dictionary.createCocktail[language]}
+                </Link>
+            </div>
         </Fragment>
     );
-}
+};
+
+export default OwnCocktail;
