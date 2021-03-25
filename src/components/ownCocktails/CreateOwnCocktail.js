dee90d87875eea0c00f958d3a4896a2684c5263a
CocoBloom <virag.baranyai2@gmail.com>
2021-03-26 00:09:00 +0100

    delete console logs

--- src/components/ownCocktails/CreateOwnCocktail.js
+++ src/components/ownCocktails/CreateOwnCocktail.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext, useState, useRef } from "react";
+import React, {Fragment, useContext, useState, useRef, useEffect} from "react";
 import { useHistory } from "react-router-dom";
 import { H1 } from "../HomeDesign";
 import { Button, Form } from "react-bootstrap";
@@ -13,6 +13,7 @@ import apiService from "../services/Api";
 export const CreateOwnCocktail = () => {
     const [errorMessage, setErrorMessage] = useState("");
     const [allIngredients] = useContext(AllIngredientsContext);
+
     const [checkedIngredients] = useContext(CheckedIngredientsContext);
     const [ownCocktails, setOwnCocktails] = useContext(OwnCocktailsContext);
     const history = useHistory();
@@ -21,7 +22,6 @@ export const CreateOwnCocktail = () => {
     const description = useRef(null);
 
     const handleSubmit = async (e) => {
-        console.log("handel submit ");
         e.preventDefault();
         const nameInput = name.current.value;
         const descriptionInput = description.current.value;
@@ -34,7 +34,6 @@ export const CreateOwnCocktail = () => {
                 strInstructions: descriptionInput,
                 ingredients: checkedIngredients,
             };
-            console.log(ownCocktail.ingredients);
             const token = localStorage.getItem("token");
             const response = await apiService.saveOwnCocktail(
                 token,
@@ -60,11 +59,12 @@ export const CreateOwnCocktail = () => {
         let result = [];
         for (const element of checkedIngredients){
             const ingredient = await apiService.getIngredientData(localStorage.getItem('token'), element);
-            console.log(ingredient);
             result.push(ingredient.data.ingredient.strIngredient);
         }
         return result;
     };
+
+    console.log(allIngredients);
 
     return (
         <Fragment>
@@ -112,14 +112,21 @@ export const CreateOwnCocktail = () => {
                     <Form.Label>
                         <h3>Cocktail ingredients</h3>
                     </Form.Label>
-                    <div className="ingredients">
-                        {allIngredients.map((ingredient) => (
-                            <Ingredient
-                                key={ingredient.idIngredient}
-                                ingredient={ingredient}
-                            />
-                        ))}
-                    </div>
+
+                    {Object.entries(allIngredients).map(([key, ingredientsByKey], index) => (
+                        <div className="ingredients" key={index}>
+                                <p className="ingredientTypeLabel">{key.toUpperCase()}: </p>
+                                {ingredientsByKey.map((ingredient) => (
+                                <Ingredient
+                                    key={ingredient.idIngredient}
+                                    ingredient={ingredient}
+                                />
+                            ))}
+                        </div>
+                    ))}
+
+
+
                 </Form.Group>
             </Form>
         </Fragment>
